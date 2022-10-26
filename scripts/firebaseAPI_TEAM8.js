4f04efebd6e0d7f651c5f983b80c4d33dca8ba52
Maximilliany <maxillian4@gmail.com>
2022-10-26 13:27:32 -0700

    removed api keys

--- scripts/firebaseAPI_TEAM8.js
+++ scripts/firebaseAPI_TEAM8.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: "AIzaSyDICNxoRBy-D1H1X4K03DvsAJr1ywz9p1s",
-    authDomain: "comp1800-bby08-216f1.firebaseapp.com",
-    projectId: "comp1800-bby08-216f1",
-    storageBucket: "comp1800-bby08-216f1.appspot.com",
-    messagingSenderId: "116773417218",
-    appId: "1:116773417218:web:7706bc7fdd48548401121b"
+    apiKey: ,
+    authDomain: ,
+    projectId: ,
+    storageBucket: ,
+    messagingSenderId: ,
+    appId: 
 };
 
 //--------------------------------------------
