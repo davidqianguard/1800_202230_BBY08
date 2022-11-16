32786b94f0dd34d6bada0be2848106e2281862d4
Maximilliany <maxillian4@gmail.com>
2022-11-16 10:19:34 -0800

    Made some changes to chart.js

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,5 +1,6 @@
 firebase.auth().onAuthStateChanged(user => {
   if (user) {
+    console.log(user.uid + " is signed in");
       getChartData(user)
   } else {
       console.log("No user is signed in");
@@ -34,38 +35,61 @@ function chart() {
       }
     });
 }
+chart()
 
-function chartData() {
-    // to check if the user is logged in:
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            console.log(user.uid); // let me to know who is the user that logged in to get the UID
+// function chartData() {
+//     // to check if the user is logged in:
+//     firebase.auth().onAuthStateChanged(user => {
+//         if (user) {
+//             console.log(user.uid); // let me to know who is the user that logged in to get the UID
 
-            // modify to get data from transactions
-            currentUser = db.collection("users").doc(user.uid).collection("transactions");
-            currentUser.get().then(userDoc => {
-                //get the user name
-                var user_Name = userDoc.data().name;
-                console.log(user_Name);
-                $("#name-goes-here").text(user_Name); //jquery
-                // document.getElementByID("name-goes-here").innetText=user_Name;
-            })
-        }
+//             // modify to get data from transactions
+//             currentUser = db.collection("users").doc(user.uid).collection("transactions");
+//             currentUser.get().then(userDoc => {
+//                 //get the user name
+//                 var user_Name = userDoc.data().name;
+//                 console.log(user_Name);
+//                 $("#name-goes-here").text(user_Name); //jquery
+//                 // document.getElementByID("name-goes-here").innetText=user_Name;
+//             })
+//         }
 
-    })
-}
+//     })
+// }
+
+// function getChartData(user) {
+//   db.collection("users").doc(user.uid).collection("transactions")
+//     .orderBy("date")
+//     .get()
+
+//     .then(allTransactions => {
+//       allTransactions.forEach(doc => {
+//         var transCategory = doc.data().category;
+//         var transCost = doc.data().cost;
+//         var transDate = doc.data().date;
+//         var transItem = doc.data().item;
+//         let tableData = TransactionTableTemplate.content.cloneNode(true);
+//         tableData.querySelector('transactionCategory').innerHTML = transCategory;
+//         tableData.querySelector('transactionCost').innerHTML = transCost;
+//         tableData.querySelector('transactionDate').innerHTML = transDate;
+//         tableData.querySelector('transactionName').innerHTML = transItem;
+
+//         TransactionGroup.appendChild(tableData);
+//       });
+//     })
+// }
 
 function getChartData(user) {
-  db.collection("users").doc(user.uid).collection(transactions)
+  db.collection("users").doc(user.uid).collection("transactions")
     .orderBy("date")
     .get()
 
     .then(allTransactions => {
       allTransactions.forEach(doc => {
         var transCategory = doc.data().category;
-        var transCost = doc.date().cost;
+        var transCost = doc.data().cost;
         var transDate = doc.data().date;
-        var transItem = doc.dat().item;
+        var transItem = doc.data().item;
         let tableData = TransactionTableTemplate.content.cloneNode(true);
         tableData.querySelector('transactionCategory').innerHTML = transCategory;
         tableData.querySelector('transactionCost').innerHTML = transCost;
