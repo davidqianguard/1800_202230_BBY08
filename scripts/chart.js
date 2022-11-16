c9b7a92c6ecc28b19ec108293bbce468c828d4dc
Maximilliany <maxillian4@gmail.com>
2022-11-16 09:36:09 -0800

    Created getChartData in chart.js to use database info

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,3 +1,11 @@
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+      getChartData(user)
+  } else {
+      console.log("No user is signed in");
+  }
+});
+
 function chart() {
     var xValues = ["Food", "Transportation", "Rent", "Auto", "Personal"];
     var yValues = [55, 49, 44, 24, 15];
@@ -26,4 +34,45 @@ function chart() {
       }
     });
 }
-chart();
+
+function chartData() {
+    // to check if the user is logged in:
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            console.log(user.uid); // let me to know who is the user that logged in to get the UID
+
+            // modify to get data from transactions
+            currentUser = db.collection("users").doc(user.uid).collection("transactions");
+            currentUser.get().then(userDoc => {
+                //get the user name
+                var user_Name = userDoc.data().name;
+                console.log(user_Name);
+                $("#name-goes-here").text(user_Name); //jquery
+                // document.getElementByID("name-goes-here").innetText=user_Name;
+            })
+        }
+
+    })
+}
+
+function getChartData(user) {
+  db.collection("users").doc(user.uid).collection(transactions)
+    .orderBy("date")
+    .get()
+
+    .then(allTransactions => {
+      allTransactions.forEach(doc => {
+        var transCategory = doc.data().category;
+        var transCost = doc.date().cost;
+        var transDate = doc.data().date;
+        var transItem = doc.dat().item;
+        let tableData = TransactionTableTemplate.content.cloneNode(true);
+        tableData.querySelector('transactionCategory').innerHTML = transCategory;
+        tableData.querySelector('transactionCost').innerHTML = transCost;
+        tableData.querySelector('transactionDate').innerHTML = transDate;
+        tableData.querySelector('transactionName').innerHTML = transItem;
+
+        TransactionGroup.appendChild(tableData);
+      });
+    })
+}
