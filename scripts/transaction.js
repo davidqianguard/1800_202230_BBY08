f28e3fa639dcb38511b867808e3beef02b3a7f89
Brian Kwon <bkwon6@my.bcit.ca>
2022-11-17 13:25:07 -0800

    changed main page layout

--- scripts/transaction.js
+++ scripts/transaction.js
@@ -1,30 +1,3 @@
-// function getTransaction() {
-//   Item = localStorage.getItem("Item");
-
-//   db.collection("transactions").where("item", "==", Item)
-//     .get()
-//     .then(queryTransaction => {
-//       //see how many results you have got from the query
-//       size = queryTransaction.size;
-//       // get the documents of query
-//       Transactions = queryTransaction.docs;
-
-//       // We want to have one document per hike, so if the the result of 
-//       //the query is more than one, we can check it right now and clean the DB if needed.
-//       // if (size = 1) {
-//       //   var thisTransaction = Transactions[0].data();
-//       //   var name = thisTransaction.name;
-//       //   document.getElementById("HikeName").innerHTML = name;
-//       // } else {
-//       //   console.log("Query has more than one data")
-//       // }
-//     })
-//     .catch((error) => {
-//       console.log("Error getting documents: ", error);
-//     });
-// }
-// // getTransaction();
-
 function addTransaction() {
   console.log("in");
   var Item = document.getElementById("item").value;
@@ -54,3 +27,24 @@ function addTransaction() {
     }
   });
 }
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
